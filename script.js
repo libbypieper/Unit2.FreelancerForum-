b8f09820c91bae10c0a9722e66ffe80eda2b72c2
John Doe <libbypieper@gmail.com>
2025-02-18 19:08:56 -0600

    freelancer

---
+++ script.js
@@ -0,0 +1,44 @@
+const list1 = document.createElement('ul');
+document.querySelector('ul').append(list1);
+
+const freelancer1name = document.createElement('li');
+freelancer1name.innerText = "Alice";
+document.querySelector('ul').append(freelancer1name);
+
+const freelancer1occupation = document.createElement('li');
+freelancer1occupation.innerText = "Writer";
+document.querySelector('li').append(freelancer1occupation);
+
+const freelancer1price = document.createElement('li');
+freelancer1price.innerText = "$30";
+document.querySelector('li').append(freelancer1price);
+
+const list2 = document.createElement('ul');
+document.querySelector('li').append(list2);
+
+const freelancer2name = document.createElement('li');
+freelancer2name.innerText = "Bob";
+document.querySelector('ul').append(freelancer2name);
+
+const freelancer2occupation = document.createElement('li');
+freelancer2occupation.innerText = "Teacher";
+document.querySelector('li').append(freelancer2occupation);
+
+const freelancer2price = document.createElement('li');
+freelancer2price.innerText = "$50";
+document.querySelector('li').append(freelancer2price);
+
+const list3 = document.createElement('ul');
+document.querySelector('li').append(list3);
+
+const freelancer3name = document.createElement('li');
+freelancer3name.innerText = "Carol";
+document.querySelector('ul').append(freelancer3name);
+
+const freelancer3occupation= document.createElement('li');
+freelancer3occupation.innerText = "Programmer";
+document.querySelector('li').append(freelancer3occupation);
+
+const freelancer3price = document.createElement('li');
+freelancer3price.innerText = "$70";
+document.querySelector('li').append(freelancer3price);
